b3fc93e209635b36663054be7b269a6faca7357d
小島秀樹 <kojimahideki@MacBook-Pro.local>
2017-08-19 20:54:06 +0900

    02-DIVE08-ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'topics#index'
   resources :topics, only: [:index, :new, :create, :edit, :update, :destroy]
 
+  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
