8886c2bee22f74a1b9dfd2fccc8dc5cfb255decf
小島秀樹 <kojimahideki@MacBook-Pro.local>
2017-08-19 19:40:18 +0900

    01-exam3作成からDIVE06まで実装

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,45 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only: [:edit, :update, :destroy]
+
+  def index
+    @topics = Topic.all
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topics_params)
+    if @topic.save
+      redirect_to topics_path, notice: "投稿しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @topic.update(topics_params)
+      redirect_to topics_path, notice: "投稿を編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to topics_path, notice: "投稿を削除しました！"
+  end
+
+  private
+    def topics_params
+      params.require(:topic).permit(:title, :content)
+    end
+
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+end
